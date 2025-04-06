01c4002cb10bcd49ead5d57dea90af4b646ec63b
Parvesh Kumar <143502925+mr-nigam@users.noreply.github.com>
2025-04-06 22:00:36 +0530

    app.py

--- app.py
+++ app.py
@@ -76,6 +76,10 @@ def chat():
     reply = nutrition_chatbot(message)
     return jsonify({"response": reply})
 
+@app.route("/")
+def home():
+    return "Nutribuddy is up and running!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port = port)
