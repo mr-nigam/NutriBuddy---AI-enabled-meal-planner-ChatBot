e1451011a4e123300ff7008bd5712653becf3827
Aakarsh7887 <150775833+Aakarsh7887@users.noreply.github.com>
2025-04-10 10:19:28 +0530

    app.py

--- app.py
+++ app.py
@@ -80,6 +80,10 @@ def chat():
 def home():
     return "Nutribuddy is up and running!"
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port = port)
